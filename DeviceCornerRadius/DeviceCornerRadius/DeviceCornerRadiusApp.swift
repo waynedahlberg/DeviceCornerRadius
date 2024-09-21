d5ca99137a705cd63762789a8c440451c274e336
Wayne Dahlberg <waynefj40@gmail.com>
2024-09-20 18:42:29 -0600

    initial commit

---
+++ DeviceCornerRadius/DeviceCornerRadius/DeviceCornerRadiusApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeviceCornerRadiusApp.swift
+//  DeviceCornerRadius
+//
+//  Created by Wayne Dahlberg on 9/20/24.
+//
+
+import SwiftUI
+
+@main
+struct DeviceCornerRadiusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
